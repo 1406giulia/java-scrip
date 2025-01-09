184f9a1c5372c0962543281bad58ab9a207012e1
Giulia Lima <js2559043@gmail.com>
2025-01-09 12:08:26 -0300

    ok

---
+++ index.js
@@ -0,0 +1,10 @@
+
+console.log("Eu funciono");
+console.log("arquivo de script EXTERNO");
+
+
+var meuNumero = 20; 
+var outroNumero = 15;
+
+console.log(meuNumero + outroNumero);
+
---
+++ teste/script.js
@@ -0,0 +1,3 @@
+for(i= 4 ; i <= 10 ; 1++){
+    console.log(i)
+}
